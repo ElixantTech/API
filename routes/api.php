69c00461c190fb72cfb22f6b018f2a6c21b8fe1a
Zander Schmautz <zander.schmautz@elixant.ca>
2018-09-23 06:23:04 -0700

    Round 4

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Http\Request;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -8,6 +10,29 @@
 | Here is where all API routes are defined.
 |
 */
+Route::get('/redirect', function () {
+    $query = http_build_query([
+        'client_id' => 'client-id',
+        'redirect_uri' => 'https://api.elixant.io/callback',
+        'response_type' => 'code',
+        'scope' => '',
+    ]);
+    
+    return redirect('https://api.elixant.io/oauth/authorize?'.$query);
+});
 
-
-
+Route::get('/callback', function (Request $request) {
+    $http = new GuzzleHttp\Client;
+    
+    $response = $http->post('https://api.elixant.io/oauth/token', [
+        'form_params' => [
+            'grant_type' => 'authorization_code',
+            'client_id' => 'client-id',
+            'client_secret' => 'client-secret',
+            'redirect_uri' => 'https://api.elixant.io/callback',
+            'code' => $request->code,
+        ],
+    ]);
+    
+    return json_decode((string) $response->getBody(), true);
+});
