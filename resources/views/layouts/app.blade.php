a2b5ef0283fc0ba9c39e1b9f336f1f772abdef0c
Zander Schmautz <zander.schmautz@elixant.ca>
2018-09-29 11:34:39 -0700

    Update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,8 +6,52 @@
         <header class="header">
             <div class="logo-container">
                 <a href="{{ url('dashboard') }}" class="logo">
-                    <img src="{{ asset('img/logo-small.png') }}" width="75" height="35" alt="{{ config('app.name') }}">
+                    <img src="{{ asset('img/logo-small.png') }}" height="35" alt="{{ config('app.name') }}">
                 </a>
+
+                <div class="d-md-none toggle-sidebar-left" data-toggle-class="sidebar-left-opened" data-target="html" data-fire-event="sidebar-left-opened">
+                    <i class="fas fa-bars" aria-label="Toggle sidebar"></i>
+                </div>
+            </div>
+
+            <div class="header-right">
+
+                <form action="#" class="search nav-form">
+                    <div class="input-group">
+                        <input type="text" class="form-control" name="q" id="q" placeholder="Search...">
+                        <span class="input-group-append">
+								<button class="btn btn-default" type="submit"><i class="fas fa-search"></i></button>
+							</span>
+                    </div>
+                </form>
+
+                <span class="separator"></span>
+
+                <ul class="notifications">
+                    <li>
+                        <a href="#" class="dropdown-toggle notifications-icon" data-toggle="dropdown">
+                            <i class="fas fa-tasks"></i>
+                        </a>
+
+                        <div class="dropdown-menu notification-menu large">
+                            <div class="notification-title">
+                                <span class="float-right badge badge-default">0</span>
+                                Active Tasks
+                            </div>
+
+                            <div class="content">
+                                <ul>
+                                    <li>
+                                        <p class="clearfix mb-1">
+                                            <span class="message center">No Active Tasks Currently..</span>
+                                        </p>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+
             </div>
         </header>
 
