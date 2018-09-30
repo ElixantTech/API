6b6b2cfd06321a1cf8cca6633c13926d72163d95
Zander Schmautz <zander.schmautz@elixant.ca>
2018-09-29 23:03:20 -0700

    5

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,21 +3,20 @@
 @section('template')
     <section class="body">
 
-        <!-- start: header -->
         <header class="header">
             <div class="logo-container">
-                <a href="../2.1.1" class="logo">
-                    <img src="img/logo.png" width="75" height="35" alt="Porto Admin" />
+                <a href="{{ url('dashboard') }}" class="logo">
+                    <img src="{{ asset('img/logo-small.png') }}" height="35" alt="{{ config('app.name') }}">
                 </a>
+
                 <div class="d-md-none toggle-sidebar-left" data-toggle-class="sidebar-left-opened" data-target="html" data-fire-event="sidebar-left-opened">
                     <i class="fas fa-bars" aria-label="Toggle sidebar"></i>
                 </div>
             </div>
 
-            <!-- start: search & user box -->
             <div class="header-right">
 
-                <form action="pages-search-results.html" class="search nav-form">
+                <form action="#" class="search nav-form">
                     <div class="input-group">
                         <input type="text" class="form-control" name="q" id="q" placeholder="Search...">
                         <span class="input-group-append">
@@ -32,45 +31,20 @@
                     <li>
                         <a href="#" class="dropdown-toggle notification-icon" data-toggle="dropdown">
                             <i class="fas fa-tasks"></i>
-                            <span class="badge">3</span>
                         </a>
 
                         <div class="dropdown-menu notification-menu large">
                             <div class="notification-title">
-                                <span class="float-right badge badge-default">3</span>
-                                Tasks
+                                <span class="float-right badge badge-default">0</span>
+                                Active Tasks
                             </div>
 
                             <div class="content">
                                 <ul>
                                     <li>
                                         <p class="clearfix mb-1">
-                                            <span class="message float-left">Generating Sales Report</span>
-                                            <span class="message float-right text-dark">60%</span>
+                                            <span class="message center">No Active Tasks Currently..</span>
                                         </p>
-                                        <div class="progress progress-xs light">
-                                            <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;"></div>
-                                        </div>
-                                    </li>
-
-                                    <li>
-                                        <p class="clearfix mb-1">
-                                            <span class="message float-left">Importing Contacts</span>
-                                            <span class="message float-right text-dark">98%</span>
-                                        </p>
-                                        <div class="progress progress-xs light">
-                                            <div class="progress-bar" role="progressbar" aria-valuenow="98" aria-valuemin="0" aria-valuemax="100" style="width: 98%;"></div>
-                                        </div>
-                                    </li>
-
-                                    <li>
-                                        <p class="clearfix mb-1">
-                                            <span class="message float-left">Uploading something big</span>
-                                            <span class="message float-right text-dark">33%</span>
-                                        </p>
-                                        <div class="progress progress-xs light mb-1">
-                                            <div class="progress-bar" role="progressbar" aria-valuenow="33" aria-valuemin="0" aria-valuemax="100" style="width: 33%;"></div>
-                                        </div>
                                     </li>
                                 </ul>
                             </div>
@@ -78,112 +52,45 @@
                     </li>
                     <li>
                         <a href="#" class="dropdown-toggle notification-icon" data-toggle="dropdown">
-                            <i class="fas fa-envelope"></i>
-                            <span class="badge">4</span>
+                            <i class="fas fa-tasks"></i>
                         </a>
 
-                        <div class="dropdown-menu notification-menu">
+                        <div class="dropdown-menu notification-menu large">
                             <div class="notification-title">
-                                <span class="float-right badge badge-default">230</span>
-                                Messages
+                                <span class="float-right badge badge-default">0</span>
+                                Active Tasks
                             </div>
 
                             <div class="content">
                                 <ul>
                                     <li>
-                                        <a href="#" class="clearfix">
-                                            <figure class="image">
-                                                <img src="img/!sample-user.jpg" alt="Joseph Doe Junior" class="rounded-circle" />
-                                            </figure>
-                                            <span class="title">Joseph Doe</span>
-                                            <span class="message">Lorem ipsum dolor sit.</span>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#" class="clearfix">
-                                            <figure class="image">
-                                                <img src="img/!sample-user.jpg" alt="Joseph Junior" class="rounded-circle" />
-                                            </figure>
-                                            <span class="title">Joseph Junior</span>
-                                            <span class="message truncate">Truncated message. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sit amet lacinia orci. Proin vestibulum eget risus non luctus. Nunc cursus lacinia lacinia. Nulla molestie malesuada est ac tincidunt. Quisque eget convallis diam, nec venenatis risus. Vestibulum blandit faucibus est et malesuada. Sed interdum cursus dui nec venenatis. Pellentesque non nisi lobortis, rutrum eros ut, convallis nisi. Sed tellus turpis, dignissim sit amet tristique quis, pretium id est. Sed aliquam diam diam, sit amet faucibus tellus ultricies eu. Aliquam lacinia nibh a metus bibendum, eu commodo eros commodo. Sed commodo molestie elit, a molestie lacus porttitor id. Donec facilisis varius sapien, ac fringilla velit porttitor et. Nam tincidunt gravida dui, sed pharetra odio pharetra nec. Duis consectetur venenatis pharetra. Vestibulum egestas nisi quis elementum elementum.</span>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#" class="clearfix">
-                                            <figure class="image">
-                                                <img src="img/!sample-user.jpg" alt="Joe Junior" class="rounded-circle" />
-                                            </figure>
-                                            <span class="title">Joe Junior</span>
-                                            <span class="message">Lorem ipsum dolor sit.</span>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#" class="clearfix">
-                                            <figure class="image">
-                                                <img src="img/!sample-user.jpg" alt="Joseph Junior" class="rounded-circle" />
-                                            </figure>
-                                            <span class="title">Joseph Junior</span>
-                                            <span class="message">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sit amet lacinia orci. Proin vestibulum eget risus non luctus. Nunc cursus lacinia lacinia. Nulla molestie malesuada est ac tincidunt. Quisque eget convallis diam.</span>
-                                        </a>
+                                        <p class="clearfix mb-1">
+                                            <span class="message center">No Active Tasks Currently..</span>
+                                        </p>
                                     </li>
                                 </ul>
-
-                                <hr />
-
-                                <div class="text-right">
-                                    <a href="#" class="view-more">View All</a>
-                                </div>
                             </div>
                         </div>
                     </li>
                     <li>
                         <a href="#" class="dropdown-toggle notification-icon" data-toggle="dropdown">
-                            <i class="fas fa-bell"></i>
-                            <span class="badge">3</span>
+                            <i class="fas fa-tasks"></i>
                         </a>
 
-                        <div class="dropdown-menu notification-menu">
+                        <div class="dropdown-menu notification-menu large">
                             <div class="notification-title">
-                                <span class="float-right badge badge-default">3</span>
-                                Alerts
+                                <span class="float-right badge badge-default">0</span>
+                                Active Tasks
                             </div>
 
                             <div class="content">
                                 <ul>
                                     <li>
-                                        <a href="#" class="clearfix">
-                                            <div class="image">
-                                                <i class="fas fa-thumbs-down bg-danger text-light"></i>
-                                            </div>
-                                            <span class="title">Server is Down!</span>
-                                            <span class="message">Just now</span>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#" class="clearfix">
-                                            <div class="image">
-                                                <i class="fas fa-lock bg-warning text-light"></i>
-                                            </div>
-                                            <span class="title">User Locked</span>
-                                            <span class="message">15 minutes ago</span>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#" class="clearfix">
-                                            <div class="image">
-                                                <i class="fas fa-signal bg-success text-light"></i>
-                                            </div>
-                                            <span class="title">Connection Restaured</span>
-                                            <span class="message">10/10/2017</span>
-                                        </a>
+                                        <p class="clearfix mb-1">
+                                            <span class="message center">No Active Tasks Currently..</span>
+                                        </p>
                                     </li>
                                 </ul>
-
-                                <hr />
-
-                                <div class="text-right">
-                                    <a href="#" class="view-more">View All</a>
-                                </div>
                             </div>
                         </div>
                     </li>
@@ -219,12 +126,25 @@
                         </ul>
                     </div>
                 </div>
-            </div>
-            <!-- end: search & user box -->
-        </header>
-        <!-- end: header -->
 
-        
+            </div>
+        </header>
+
+        <div class="inner-wrapper">
+
+            <aside id="sidebar-left" class="sidebar-left">
+
+            </aside>
+
+            <section role="main" class="content-body">
+
+                <main class="py-4">
+                    @yield('content')
+                </main>
+
+            </section>
+
+        </div>
 
     </section>
 @endsection
