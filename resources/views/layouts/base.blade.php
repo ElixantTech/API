86874edeafa5eb8559ba8bd15b4153fc91826732
Zander Schmautz <zander.schmautz@elixant.ca>
2018-09-30 00:05:58 -0700

    2

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -13,7 +13,7 @@
 
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800|Shadows+Into+Light" rel="stylesheet" type="text/css">
 
-    <link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.css" />
+    <link rel="stylesheet" href="{{ asset('vendor/bootstrap/css/bootstrap.css') }}" />
     <link rel="stylesheet" href="{{ asset('vendor/animate/animate.css') }}">
 
     <link rel="stylesheet" href="{{ asset('vendor/font-awesome/css/fontawesome-all.min.css') }}" />
@@ -24,13 +24,13 @@
     <link href="{{ asset('css/theme.css') }}" rel="stylesheet">
     <link href="{{ asset('css/skins/default.css') }}" rel="stylesheet">
 
-    <script src="vendor/modernizr/modernizr.js"></script>
+    <script src="{{ asset('js/app.js') }}"></script>
+
+    <script src="{{ asset('vendor/modernizr/modernizr.js') }}"></script>
 </head>
 <body>
 
 @yield('template')
-
-<script src="{{ asset('js/app.js') }}"></script>
 
 <script src="{{ asset('vendor/jquery-browser-mobile/jquery.browser.mobile.js') }}"></script>
 <script src="{{ asset('vendor/popper/umd/popper.min.js') }}"></script>
