754373794ea1c5bae6dd38b6d008c73987550fd3
Zander Schmautz <zander.schmautz@elixant.ca>
2018-09-30 06:48:57 -0700

    woops3

--- public/js/app.js
+++ public/js/app.js
@@ -47839,7 +47839,7 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 //
 
 /* harmony default export */ __webpack_exports__["default"] = ({
-    name: "LoginPageForm",
+    name: "login-page-form",
     props: ["destination"]
 });
 
