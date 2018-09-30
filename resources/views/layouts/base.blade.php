77a23286cda5e4776a9bf82023afde099a573317
Zander Schmautz <zander.schmautz@elixant.ca>
2018-09-30 00:06:53 -0700

    3

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -24,7 +24,7 @@
     <link href="{{ asset('css/theme.css') }}" rel="stylesheet">
     <link href="{{ asset('css/skins/default.css') }}" rel="stylesheet">
 
-    <script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{ asset('js/app.js') }}" defer></script>
 
     <script src="{{ asset('vendor/modernizr/modernizr.js') }}"></script>
 </head>
