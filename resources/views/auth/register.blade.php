04775f219c2090c044bba3408c808d37c94d8b8d
Zander Schmautz <zander.schmautz@elixant.ca>
2018-09-30 11:53:32 -0700

    Part 1

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -68,13 +68,21 @@
                                     </div>
                                 </div>
 
+                                <div class="clearfix"></div>
+
                                 <div class="form-group row">
-                                    <label class="col-sm-4 control-label text-sm-right pt-1" for="w2-password">Password</label>
-                                    <div class="col-sm-8">
-                                        <input class="form-control" name="password" id="w2-password" required="" minlength="6" type="password">
+                                    <div class="col-md-6">
+                                        {{ Form::label('password', 'Password', ['class' => 'col_form_label']) }}
+                                        {{ Form::password('password', '', ['class' => 'form-control form-control-lg', 'required' => 'required']) }}
+                                    </div>
+                                    <div class="col-md-6">
+                                        {{ Form::label('password_confirmation', 'Confirm Password', ['class' => 'col_form_label']) }}
+                                        {{ Form::password('password_confirmation', '', ['class' => 'form-control form-control-lg', 'required' => 'required']) }}
                                     </div>
                                 </div>
                             </div>
+
+
                             <div id="signup-profile" class="tab-pane p-3">
                                 <div class="form-group row">
                                     <label class="col-sm-4 control-label text-sm-right pt-1" for="w2-first-name">First Name</label>
@@ -89,9 +97,13 @@
                                     </div>
                                 </div>
                             </div>
-                            <div id="signup-profile" class="tab-pane p-3"></div>
+
                             <div id="signup-company" class="tab-pane p-3"></div>
+
+
                             <div id="signup-billing" class="tab-pane p-3"></div>
+
+
                             <div id="w2-confirm" class="tab-pane p-3">
                                 <div class="form-group row">
                                     <label class="col-sm-4 control-label text-sm-right pt-1" for="w2-email">Email</label>
@@ -99,6 +111,8 @@
                                         <input class="form-control" name="email" id="w2-email" required="" type="text">
                                     </div>
                                 </div>
+
+
                                 <div class="form-group row">
                                     <div class="col-sm-2"></div>
                                     <div class="col-sm-10">
@@ -126,128 +140,6 @@
                     </ul>
                 </div>
             </section>
-
-            <div class="panel card-sign">
-                <div class="card-title-sign mt-3 text-right">
-                    <h2 class="title text-uppercase font-weight-bold m-0">
-                        <i class="fas fa-user mr-1"></i> Sign Up
-                    </h2>
-                </div>
-
-                <div class="card-body">
-                    <form method="post" id="login">
-
-                        <div class="row form-group mb-3">
-                            <div class="col-md-6">
-                                <label class="col-form-label" for="first_name">First Name</label>
-                                <input type="text" name="first_name" id="first_name"
-                                       class="form-control form-control-lg" value="{{ old('first_name') }}">
-                            </div>
-                            <div class="col-md-6">
-                                <label class="col-form-label" for="last_name">First Name</label>
-                                <input type="text" name="last_name" id="last_name" class="form-control form-control-lg"
-                                       value="{{ old('last_name') }}">
-                            </div>
-                        </div>
-
-                        <div class="clearfix"></div>
-
-                        <div class="form-group mb-3">
-                            <label for="company_name">Company Name:</label>
-                            <input name="company_name" type="text" id="company_name"
-                                   class="form-control form-control-lg{{ $errors->has('company_name') ? ' is-invalid' : '' }}"
-                                   value="{{ old('email') }}" required/>
-
-                                @if ($errors->has('company_name'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('company_name') }}</strong>
-                                    </span>
-                                @endif
-                        </div>
-
-                        <div class="clearfix"></div>
-
-                        <div class="row form-group mb-3">
-                            <div class="col-md-8">
-                                <label class="col-form-label" for="street_address">Company Address:</label>
-                                <input type="text" name="street_address" id="street_address"
-                                       class="form-control form-control-lg" value="{{ old('street_address') }}" required>
-                            </div>
-                            <div class="col-md-4">
-                                <label class="col-form-label" for="city">City:</label>
-                                <input type="text" name="city" id="city" class="form-control form-control-lg"
-                                       value="{{ old('city') }}" required>
-                            </div>
-                        </div>
-
-                        <div class="clearfix"></div>
-
-                        <div class="form-group mb-3">
-                            <label for="email">Company E-Mail Address:</label>
-                            <div class="input-group">
-                                <input name="email" type="email" id="email"
-                                       class="form-control form-control-lg{{ $errors->has('email') ? ' is-invalid' : '' }}"
-                                       value="{{ old('email') }}" email required/>
-                                <span class="input-group-append">
-                            <span class="input-group-text">
-                            <i class="fas fa-envelope"></i>
-                    </span>
-                </span>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="clearfix"></div>
-
-                        <div class="form-group mb-3">
-                            <label for="password">Password:</label>
-                            <div class="input-group">
-                                <input name="password" type="password" id="password"
-                                       class="form-control form-control-lg" required/>
-                                <span class="input-group-append">
-                    <span class="input-group-text">
-                        <i class="fas fa-key"></i>
-                    </span>
-                </span>
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="row">
-                            <div class="col-sm-8">
-                                <div class="checkbox-custom checkbox-default">
-                                    <input id="RememberMe" name="rememberme" type="checkbox"/>
-                                    <label for="RememberMe">Remember Me</label>
-                                </div>
-                            </div>
-                            <div class="col-sm-4 text-right">
-                                <button type="submit" class="btn btn-primary mt-2">Sign In</button>
-                            </div>
-                        </div>
-
-                        <span class="mt-3 mb-3 line-thru text-center text-uppercase">
-								<span>or</span>
-							</span>
-
-                        <div class="mb-1 text-center">
-                            <a class="btn btn-facebook mb-3 ml-1 mr-1" href="#">Connect with <i
-                                    class="fab fa-facebook-f"></i></a>
-                        </div>
-
-                        <p class="text-center">Don't have an account yet? <a href="pages-signup.html">Sign Up!</a></p>
-                    </form>
-                </div>
-            </div>
         </div>
     </section>
 @endsection
