99a99c014320d2dbc73d80019c7b6704b9be84bd
Zander Schmautz <zander.schmautz@elixant.ca>
2018-09-29 23:57:32 -0700

    ooo

---
+++ resources/views/layouts/app-header-userbox.blade.php
@@ -0,0 +1,28 @@
+<div id="userbox" class="userbox">
+    <a href="#" data-toggle="dropdown">
+        <figure class="profile-picture">
+            <img src="img/!logged-user.jpg" alt="Joseph Doe" class="rounded-circle" data-lock-picture="img/!logged-user.jpg" />
+        </figure>
+        <div class="profile-info" data-lock-name="John Doe" data-lock-email="johndoe@okler.com">
+            <span class="name">John Doe Junior</span>
+            <span class="role">administrator</span>
+        </div>
+
+        <i class="fa custom-caret"></i>
+    </a>
+
+    <div class="dropdown-menu">
+        <ul class="list-unstyled mb-2">
+            <li class="divider"></li>
+            <li>
+                <a role="menuitem" tabindex="-1" href="pages-user-profile.html"><i class="fas fa-user"></i> My Profile</a>
+            </li>
+            <li>
+                <a role="menuitem" tabindex="-1" href="#" data-lock-screen="true"><i class="fas fa-lock"></i> Lock Screen</a>
+            </li>
+            <li>
+                <a role="menuitem" tabindex="-1" href="pages-signin.html"><i class="fas fa-power-off"></i> Logout</a>
+            </li>
+        </ul>
+    </div>
+</div>
