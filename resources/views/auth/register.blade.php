d406b8c7a6a199ff89f617e497967df8786997b8
Zander Schmautz <zander.schmautz@elixant.ca>
2018-09-30 10:58:33 -0700

    tst

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -7,6 +7,108 @@
                 <img src="{{ asset('img/logo.png') }}" height="54"/>
             </a>
 
+            <section class="card form-wizard" id="signup">
+                <div class="tabs">
+                    <ul class="nav nav-tabs nav-justify wizard-steps">
+                        <li class="nav-item active">
+                            <a href="#signup-account" data-toggle="tab" class="nav-link text-center">
+                                <span class="badge badge-primary">1</span>
+                                Account
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#signup-profile" data-toggle="tab" class="nav-link text-center">
+                                <span class="badge badge-primary">2</span>
+                                Profile
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#signup-company" data-toggle="tab" class="nav-link text-center">
+                                <span class="badge badge-primary">3</span>
+                                Company
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#signup-billing" data-toggle="tab" class="nav-link text-center">
+                                <span class="badge badge-primary">4</span>
+                                Billing
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#w2-confirm" data-toggle="tab" class="nav-link text-center">
+                                <span class="badge badge-primary">5</span>
+                                Confirm
+                            </a>
+                        </li>
+                    </ul>
+                    <form class="form-horizontal" novalidate="novalidate">
+                        <div class="tab-content">
+                            <div id="w2-account" class="tab-pane p-3 active">
+                                <div class="form-group row">
+                                    <label class="col-sm-4 control-label text-sm-right pt-1" for="w2-username">Username</label>
+                                    <div class="col-sm-8">
+                                        <input class="form-control" id="w2-username" name="username" required="" type="text">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label class="col-sm-4 control-label text-sm-right pt-1" for="w2-password">Password</label>
+                                    <div class="col-sm-8">
+                                        <input class="form-control" name="password" id="w2-password" required="" minlength="6" type="password">
+                                    </div>
+                                </div>
+                            </div>
+                            <div id="signup-profile" class="tab-pane p-3">
+                                <div class="form-group row">
+                                    <label class="col-sm-4 control-label text-sm-right pt-1" for="w2-first-name">First Name</label>
+                                    <div class="col-sm-8">
+                                        <input class="form-control" name="first-name" id="w2-first-name" required="" type="text">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label class="col-sm-4 control-label text-sm-right pt-1" for="w2-last-name">Last Name</label>
+                                    <div class="col-sm-8">
+                                        <input class="form-control" name="last-name" id="w2-last-name" required="" type="text">
+                                    </div>
+                                </div>
+                            </div>
+                            <div id="signup-profile" class="tab-pane p-3"></div>
+                            <div id="signup-company" class="tab-pane p-3"></div>
+                            <div id="signup-billing" class="tab-pane p-3"></div>
+                            <div id="w2-confirm" class="tab-pane p-3">
+                                <div class="form-group row">
+                                    <label class="col-sm-4 control-label text-sm-right pt-1" for="w2-email">Email</label>
+                                    <div class="col-sm-8">
+                                        <input class="form-control" name="email" id="w2-email" required="" type="text">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <div class="col-sm-2"></div>
+                                    <div class="col-sm-10">
+                                        <div class="checkbox-custom">
+                                            <input name="terms" id="w2-terms" required="" type="checkbox">
+                                            <label for="w2-terms">I agree to the terms of service</label>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                <div class="card-footer">
+                    <ul class="pager">
+                        <li class="previous disabled">
+                            <a><i class="fas fa-angle-left"></i> Previous</a>
+                        </li>
+                        <li class="finish hidden float-right">
+                            <a>Finish</a>
+                        </li>
+                        <li class="next">
+                            <a>Next <i class="fas fa-angle-right"></i></a>
+                        </li>
+                    </ul>
+                </div>
+            </section>
+
             <div class="panel card-sign">
                 <div class="card-title-sign mt-3 text-right">
                     <h2 class="title text-uppercase font-weight-bold m-0">
