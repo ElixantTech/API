df926e4d89a7184a57be8eb2da6c7a4d07e2ecbf
Zander Schmautz <zander.schmautz@elixant.ca>
2018-10-06 08:01:52 -0700

    hmmm

--- routes/api.php
+++ routes/api.php
@@ -17,5 +17,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::middleware('cors')
-    ->post('user/emailTaken', 'UserController@verifyEmailExists');
+Route::any('user/emailTaken', 'UserController@verifyEmailExists');
