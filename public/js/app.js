7b170bb35e3877ece5ad85256f0c3dca7ad1e17f
Zander Schmautz <zander.schmautz@elixant.ca>
2018-10-06 11:16:54 -0700

    lol

--- public/js/app.js
+++ public/js/app.js
@@ -71152,73 +71152,6 @@ $(document).ready(function () {
         });
     };
 
-    var autocomplete;
-
-    function initAutocomplete() {
-        // Create the autocomplete object, restricting the search to geographical
-        // location types.
-        autocomplete = new google.maps.places.Autocomplete(
-        /** @type {!HTMLInputElement} */document.getElementById('address'), { types: ['geocode'] });
-
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(function (position) {
-                var geolocation = {
-                    lat: position.coords.latitude,
-                    lng: position.coords.longitude
-                };
-                var circle = new google.maps.Circle({
-                    center: geolocation,
-                    radius: position.coords.accuracy
-                });
-                autocomplete.setBounds(circle.getBounds());
-            });
-        }
-
-        // When the user selects an address from the dropdown, populate the address
-        // fields in the form.
-        autocomplete.addListener('place_changed', fillInAddress);
-    }
-
-    function fillInAddress() {
-        // Get the place details from the autocomplete object.
-        var place = autocomplete.getPlace();
-
-        var street_num = '';
-        var street_name = '';
-
-        // Get each component of the address from the place details
-        // and fill the corresponding field on the form.
-        for (var i = 0; i < place.address_components.length; i++) {
-            var addressType = place.address_components[i].types[0];
-
-            if (addressType === 'street_number') {
-                street_num = place.address_components[i]['short_name'];
-            }
-            elseif(addressType === 'route');
-            {
-                street_name = place.address_components[i]['long_name'];
-            }
-            elseif(addressType === 'locality');
-            {
-                $("#city").val(place.address_components[i]['long_name']);
-            }
-            elseif(addressType === 'administrative_area_level_1');
-            {
-                $("#province").val(place.address_components[i]['long_name']);
-            }
-            elseif(addressType === 'country');
-            {
-                $("#country").val(place.address_components[i]['long_name']);
-            }
-            elseif(addressType === 'postal_code');
-            {
-                $("#postal_code").val(place.address_components[i]['long_name']);
-            }
-
-            $("#address").val(street_num + ' ' + street_name);
-        }
-    }
-
     $('.select-plan').click(function () {
         console.log('click!');
 
@@ -71228,6 +71161,73 @@ $(document).ready(function () {
         $('#plan-field').val($(this).attr('id'));
     });
 });
+
+var autocomplete;
+
+function initAutocomplete() {
+    // Create the autocomplete object, restricting the search to geographical
+    // location types.
+    autocomplete = new google.maps.places.Autocomplete(
+    /** @type {!HTMLInputElement} */document.getElementById('address'), { types: ['geocode'] });
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            var geolocation = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+            var circle = new google.maps.Circle({
+                center: geolocation,
+                radius: position.coords.accuracy
+            });
+            autocomplete.setBounds(circle.getBounds());
+        });
+    }
+
+    // When the user selects an address from the dropdown, populate the address
+    // fields in the form.
+    autocomplete.addListener('place_changed', fillInAddress);
+}
+
+function fillInAddress() {
+    // Get the place details from the autocomplete object.
+    var place = autocomplete.getPlace();
+
+    var street_num = '';
+    var street_name = '';
+
+    // Get each component of the address from the place details
+    // and fill the corresponding field on the form.
+    for (var i = 0; i < place.address_components.length; i++) {
+        var addressType = place.address_components[i].types[0];
+
+        if (addressType === 'street_number') {
+            street_num = place.address_components[i]['short_name'];
+        }
+        elseif(addressType === 'route');
+        {
+            street_name = place.address_components[i]['long_name'];
+        }
+        elseif(addressType === 'locality');
+        {
+            $("#city").val(place.address_components[i]['long_name']);
+        }
+        elseif(addressType === 'administrative_area_level_1');
+        {
+            $("#province").val(place.address_components[i]['long_name']);
+        }
+        elseif(addressType === 'country');
+        {
+            $("#country").val(place.address_components[i]['long_name']);
+        }
+        elseif(addressType === 'postal_code');
+        {
+            $("#postal_code").val(place.address_components[i]['long_name']);
+        }
+
+        $("#address").val(street_num + ' ' + street_name);
+    }
+}
 
 /***/ }),
 /* 73 */
