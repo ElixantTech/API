1ca065cef95e467ec7047387efe61c7f65acc8eb
Zander Schmautz <zander.schmautz@elixant.ca>
2018-10-11 07:24:38 -0700

    1

--- webpack.mix.js
+++ webpack.mix.js
@@ -19,7 +19,7 @@ mix
     .styles([
         'resources/js/vendor/bootstrap/css/bootstrap.css',
         'resources/js/vendor/animate/animate.css',
-        'resources/js/vendor/font-awesome/css/fontawesome-all.min.css',
+        'node_modules/font-awesome/css/font-awesome.css',
         'resources/js/vendor/magnific-popup/magnific-popup.css',
         'resources/js/vendor/bootstrap-datepicker/css/bootstrap-datepicker3.css'
     ], 'public/css/vendor.bundle.css')
